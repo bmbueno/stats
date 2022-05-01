5538cad15ca7a315713031e612ed567e2514eac6
Bruno Morais <bruno.morais@inf.ufrgs.br>
2022-05-01 02:33:03 -0300

    Add button bio artist

--- src/components/molecules/ArtistWithGenres.jsx
+++ src/components/molecules/ArtistWithGenres.jsx
@@ -5,16 +5,23 @@ import Genres from "../organisms/Genres";
 
 const ArtistWithGenres = (props) => {
     const [ bio, setBio] = useState('')
+    const [ bioOpen, setBioOpen ] = useState(false)
     useEffect(() => {
         setBio(props.artist.bio.summary.split("<a")[0])
     }, [props.artist.bio])
+
+    const handleClick = () => {
+        setBioOpen(!bioOpen)
+    }
 
     return (
         <>
             { props.artist.genres.length > 0 ? 
                 <>
-                    <Name> - {props.artist.name}</Name>
-                    <Bio> {bio} </Bio>
+                    <Name> - {props.artist.name} { bio ? <OpenBio onClick={handleClick} >Bio</OpenBio> : ''} </Name>
+                    <Collapse open={bioOpen} >
+                        <Bio> {bio} </Bio>
+                    </Collapse>
                     <Genres list={props.artist.genres}  />
                 </>
                 : <></> 
@@ -31,6 +38,23 @@ const Name  = styled.h3`
 `;
 
 const Bio  = styled.p`
+    margin: 0px;
     color: white;
     font-size: 18px;
+`;
+
+const OpenBio = styled.a`
+    border-radius: 30%;
+    margin-left: 10px;
+    padding: 2px 5px;
+    background-color: #1DB954;
+    color: white;
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const Collapse = styled.div`
+    max-height: ${props => props.open ? "100%" : "0"};
+    overflow: hidden;
+    transition: all 0.3s ease-out;
 `;
